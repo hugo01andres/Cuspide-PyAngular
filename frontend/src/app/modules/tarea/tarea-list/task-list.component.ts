0c13bf6e134f3d97db11537ac45007a27fa745ac
hugo01andres <hugoandres01@live.com.mx>
2023-12-12 13:11:54 -0600

    List component complete

--- frontend/src/app/modules/tarea/tarea-list/task-list.component.ts
+++ frontend/src/app/modules/tarea/tarea-list/task-list.component.ts
@@ -3,15 +3,20 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TaskService } from '../task.service';
 import { TaskList } from '../tarea.types';
 import { MenuComponent } from '../../../shared/menu/menu/menu.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule, NgFor } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [MatSlideToggleModule, MenuComponent],
+  imports: [MatSlideToggleModule, MenuComponent, MatTableModule, MatIconModule, CommonModule, MatButtonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent implements OnInit{
   listTasks : TaskList[] = [];
+  columnsToDisplay = ['id', 'name', 'action'];
   constructor(private _taskService : TaskService) { }
   ngOnInit(): void {
     this.getTaskList();
